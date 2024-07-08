b4c6894dc0ac5b14c27564eeecc7f373472fe682
Kris <kristo190@hotmail.com>
2024-07-08 17:42:14 +0300

    feat: erase sensitive parts from OPEN_URL packet
    The sensitive parts will now be stripped in
    https://secure.runescape.com/* URLs, removing anything at the asterisk.
    This is because these URLs frequently contain tokens to log in to the
    site. Any URL that is not 'secure.runescape.com' will be kept in its
    entirety, however.

--- proxy/src/main/kotlin/net/rsprox/proxy/server/ServerGameHandler.kt
+++ proxy/src/main/kotlin/net/rsprox/proxy/server/ServerGameHandler.kt
@@ -1,5 +1,6 @@
 package net.rsprox.proxy.server
 
+import io.netty.buffer.Unpooled
 import io.netty.channel.Channel
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
@@ -7,6 +8,7 @@ import net.rsprot.buffer.extensions.gjstr
 import net.rsprot.buffer.extensions.pjstr
 import net.rsprox.proxy.binary.StreamDirection
 import net.rsprox.proxy.channel.getBinaryBlob
+import net.rsprox.proxy.channel.getServerToClientStreamCipher
 import net.rsprox.proxy.huffman.HuffmanProvider
 import net.rsprox.proxy.server.prot.GameServerProt
 
@@ -44,6 +46,37 @@ public class ServerGameHandler(
             // Replace the contents of private messages with asterisks of the same msg length
             huffman.encode(replacement, "*".repeat(contents.length))
             msg.replacePayload(replacement)
+            return
         }
+        if (msg.prot == GameServerProt.URL_OPEN) {
+            val array = ByteArray(msg.payload.readableBytes())
+            msg.payload.readBytes(array)
+            val isaac = ctx.channel().getServerToClientStreamCipher()
+            for (i in array.indices) {
+                array[i] = (array[i].toInt() - isaac.nextInt()).toByte()
+            }
+            val buf = Unpooled.wrappedBuffer(array)
+            val url = buf.gjstr()
+            // Secure RuneScape URLs follow the pattern seen below:
+            // https://secure.runescape.com/m=weblogin
+            // /s=*/p=*/redirect.ws?rpt=1&mod=news&dest=runefest---new-date-announced
+            // ^These URLs contain sensitive data, and as such, we strip anything after the
+            // secure.runescape.com/ part. This ensures we still get insight about what the URL was,
+            // but not any of the potential credentials that come with it.
+            val safeUrl =
+                url.replace(SENSITIVE_URL_REGEX) { match ->
+                    val (sensitiveSuffix) = match.destructured
+                    // Take the sensitive suffix of the URL and replace it with just asterisks
+                    match.value.replace(sensitiveSuffix, "*".repeat(sensitiveSuffix.length))
+                }
+            val output = ctx.alloc().buffer(safeUrl.length + 1)
+            output.pjstr(safeUrl)
+            msg.replacePayload(output)
+            return
+        }
+    }
+
+    private companion object {
+        private val SENSITIVE_URL_REGEX: Regex = Regex("""^https?://(?:www\.)?secure\.runescape\.com/(.*)$""")
     }
 }
