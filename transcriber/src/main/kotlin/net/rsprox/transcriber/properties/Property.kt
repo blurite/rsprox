c94953c8376722a8374a6ce0f57d7030ce520f03
Kris <kristo190@hotmail.com>
2024-07-29 20:13:36 +0300

    refactor: delete old properties/formatters

--- transcriber/src/main/kotlin/net/rsprox/transcriber/properties/Property.kt
+++
@@ -1,6 +0,0 @@
-package net.rsprox.transcriber.properties
-
-public data class Property(
-    public val name: String,
-    public val value: Any,
-)
--- transcriber/transcriber-223/src/main/kotlin/net/rsprox/transcriber/SpecificMessageFormatterExtensions.kt
+++
@@ -1,7 +0,0 @@
-package net.rsprox.transcriber
-
-import net.rsprox.protocol.common.CoordGrid
-
-public fun MessageFormatter.coord(coordGrid: CoordGrid): String {
-    return coord(coordGrid.level, coordGrid.x, coordGrid.z)
-}
