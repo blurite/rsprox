665fdae52a3583f8f6424c77930fc5fa86b656ea
Kris <kristo190@hotmail.com>
2024-08-07 10:49:56 +0300

    fix: prevent modifying header checkboxes for default preset

--- gui/proxy-tool/src/main/kotlin/net/rsprox/gui/FiltersSidePanel.kt
+++ gui/proxy-tool/src/main/kotlin/net/rsprox/gui/FiltersSidePanel.kt
@@ -16,9 +16,8 @@ import java.awt.event.MouseEvent
 import javax.swing.*
 
 public class FiltersSidePanel(
-    private val proxyService: ProxyService
+    private val proxyService: ProxyService,
 ) : JPanel() {
-
     private val presetsBoxModel = DefaultComboBoxModel<String>()
     private val presetsBox = JComboBox(presetsBoxModel)
     private val copyButton = createControlButton(AppIcons.Copy, "Copy selected preset into new preset")
@@ -39,8 +38,9 @@ public class FiltersSidePanel(
         }
 
         createButton.addActionListener {
-            val name = Dialogs.showInputString(parent = createButton, "Create new preset", "Enter preset name")
-                ?: return@addActionListener
+            val name =
+                Dialogs.showInputString(parent = createButton, "Create new preset", "Enter preset name")
+                    ?: return@addActionListener
             if (presetsBoxModel.getIndexOf(name) != -1) {
                 Dialogs.showError(parent = this, message = "Preset with name '$name' already exists.")
                 return@addActionListener
@@ -54,13 +54,14 @@ public class FiltersSidePanel(
             val selectedIndex = presetsBox.selectedIndex
             if (selectedIndex == -1) return@addActionListener
             val presetName = presetsBox.selectedItem as String
-            val result = JOptionPane.showConfirmDialog(
-                deleteButton,
-                "Are you sure you want to delete the selected preset?",
-                "Delete '${presetName}' preset",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.WARNING_MESSAGE
-            )
+            val result =
+                JOptionPane.showConfirmDialog(
+                    deleteButton,
+                    "Are you sure you want to delete the selected preset?",
+                    "Delete '$presetName' preset",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.WARNING_MESSAGE,
+                )
             if (result != JOptionPane.YES_OPTION) return@addActionListener
             proxyService.filterSetStore.delete(selectedIndex)
             populatePresets()
@@ -69,8 +70,9 @@ public class FiltersSidePanel(
         copyButton.addActionListener {
             val selectedIndex = presetsBox.selectedIndex
             if (selectedIndex == -1) return@addActionListener
-            val name = Dialogs.showInputString(parent = copyButton, "Copy preset", "Enter new preset name")
-                ?: return@addActionListener
+            val name =
+                Dialogs.showInputString(parent = copyButton, "Copy preset", "Enter new preset name")
+                    ?: return@addActionListener
             if (presetsBoxModel.getIndexOf(name) != -1) {
                 Dialogs.showError(parent = this, message = "Preset with name '$name' already exists.")
                 return@addActionListener
@@ -88,12 +90,13 @@ public class FiltersSidePanel(
 
         add(presetsBox, "growx")
 
-        val controlPanel = JPanel().apply {
-            layout = MigLayout("insets 0", "[grow][grow][grow]", "[32px]")
-            add(copyButton, "grow, hmin 32px")
-            add(createButton, "grow, hmin 32px")
-            add(deleteButton, "grow, hmin 32px")
-        }
+        val controlPanel =
+            JPanel().apply {
+                layout = MigLayout("insets 0", "[grow][grow][grow]", "[32px]")
+                add(copyButton, "grow, hmin 32px")
+                add(createButton, "grow, hmin 32px")
+                add(deleteButton, "grow, hmin 32px")
+            }
 
         add(controlPanel, "growx")
 
@@ -101,23 +104,25 @@ public class FiltersSidePanel(
         tabbedGroup.addTab("Incoming", incomingPanel.wrapWithBorderlessScrollPane())
         tabbedGroup.addTab("Outgoing", outgoingPanel.wrapWithBorderlessScrollPane())
 
-        tabbedGroup.addMouseListener(object : MouseAdapter() {
-            override fun mousePressed(e: MouseEvent) {
-                if (!SwingUtilities.isRightMouseButton(e)) return
-                if (presetsBox.selectedIndex == 0) return
-                val tabIndex = tabbedGroup.ui.tabForCoordinate(tabbedGroup, e.x, e.y)
-                if (tabIndex == -1) return
-                val panel = if (tabIndex == 0) incomingPanel else outgoingPanel
-                val menu = JPopupMenu()
-                val enableAll = JMenuItem("Enable All")
-                val disableAll = JMenuItem("Disable All")
-                enableAll.addActionListener { panel.setAll(true) }
-                disableAll.addActionListener { panel.setAll(false) }
-                menu.add(enableAll)
-                menu.add(disableAll)
-                menu.show(e.component, e.x, e.y)
-            }
-        })
+        tabbedGroup.addMouseListener(
+            object : MouseAdapter() {
+                override fun mousePressed(e: MouseEvent) {
+                    if (!SwingUtilities.isRightMouseButton(e)) return
+                    if (presetsBox.selectedIndex == 0) return
+                    val tabIndex = tabbedGroup.ui.tabForCoordinate(tabbedGroup, e.x, e.y)
+                    if (tabIndex == -1) return
+                    val panel = if (tabIndex == 0) incomingPanel else outgoingPanel
+                    val menu = JPopupMenu()
+                    val enableAll = JMenuItem("Enable All")
+                    val disableAll = JMenuItem("Disable All")
+                    enableAll.addActionListener { panel.setAll(true) }
+                    disableAll.addActionListener { panel.setAll(false) }
+                    menu.add(enableAll)
+                    menu.add(disableAll)
+                    menu.show(e.component, e.x, e.y)
+                }
+            },
+        )
 
         add(tabbedGroup, "grow, pushy")
 
@@ -133,7 +138,6 @@ public class FiltersSidePanel(
         }
         incomingPanel.updateAllHeaderCheckboxes()
         outgoingPanel.updateAllHeaderCheckboxes()
-
     }
 
     private fun updateButtonState() {
@@ -162,30 +166,38 @@ public class FiltersSidePanel(
         }
     }
 
-    private fun createControlButton(icon: Icon, tooltip: String) = FlatButton().apply {
+    private fun createControlButton(
+        icon: Icon,
+        tooltip: String,
+    ) = FlatButton().apply {
         this.buttonType = ButtonType.square
         this.icon = icon
         this.toolTipText = tooltip
         isFocusPainted = false
     }
 
-    private inner class FiltersPanel(private val direction: StreamDirection) : JPanel() {
-
+    private inner class FiltersPanel(
+        private val direction: StreamDirection,
+    ) : JPanel() {
         private val headerCheckboxes = hashMapOf<ProtCategory, FlatTriStateCheckBox>()
 
         init {
             layout = MigLayout("flowy, ins 0, gap 0", "[grow]", "[]")
 
-            val filteredProperties = PropertyFilter.entries
-                .filter { it.direction == direction }
-                .groupBy { it.category }
+            val filteredProperties =
+                PropertyFilter.entries
+                    .filter { it.direction == direction }
+                    .groupBy { it.category }
 
             for ((category, properties) in filteredProperties) {
                 add(createCategoryPanel(category, properties), "growx")
             }
         }
 
-        private fun createCategoryPanel(category: ProtCategory, properties: List<PropertyFilter>) = JPanel().apply {
+        private fun createCategoryPanel(
+            category: ProtCategory,
+            properties: List<PropertyFilter>,
+        ) = JPanel().apply {
             layout = BoxLayout(this, BoxLayout.Y_AXIS)
 
             val content = JPanel()
@@ -206,18 +218,19 @@ public class FiltersSidePanel(
         private fun createCategoryHeaderPanel(
             content: JPanel,
             category: ProtCategory,
-            properties: List<PropertyFilter>
+            properties: List<PropertyFilter>,
         ) = JPanel(BorderLayout()).apply {
             val toggle = FlatButton()
             toggle.toolTipText = "Collapse"
             toggle.icon = AppIcons.Collapse
             toggle.buttonType = ButtonType.toolBarButton
 
-            val collapseAction = ActionListener {
-                content.isVisible = !content.isVisible
-                toggle.icon = if (content.isVisible) AppIcons.Collapse else AppIcons.Expand
-                toggle.toolTipText = if (content.isVisible) "Collapse" else "Expand"
-            }
+            val collapseAction =
+                ActionListener {
+                    content.isVisible = !content.isVisible
+                    toggle.icon = if (content.isVisible) AppIcons.Collapse else AppIcons.Expand
+                    toggle.toolTipText = if (content.isVisible) "Collapse" else "Expand"
+                }
 
             toggle.addActionListener(collapseAction)
             add(toggle, BorderLayout.WEST)
@@ -244,36 +257,46 @@ public class FiltersSidePanel(
             add(checkbox, BorderLayout.EAST)
             headerCheckboxes[category] = checkbox
 
-            label.addMouseListener(object : MouseAdapter() {
-
-                override fun mouseEntered(e: MouseEvent?) {
-                    label.foreground = UIManager.getColor("Label.selectedForeground")
-                }
-
-                override fun mouseExited(e: MouseEvent?) {
-                    label.foreground = UIManager.getColor("Label.foreground")
-                }
-
-                override fun mouseReleased(e: MouseEvent) {
-                    if (SwingUtilities.isLeftMouseButton(e)
-                        && e.x >= 0 && e.x <= label.width && e.y >= 0 && e.y <= label.height
-                    ) {
-                        collapseAction.actionPerformed(null)
+            label.addMouseListener(
+                object : MouseAdapter() {
+                    override fun mouseEntered(e: MouseEvent?) {
+                        label.foreground = UIManager.getColor("Label.selectedForeground")
                     }
-                }
-            })
+
+                    override fun mouseExited(e: MouseEvent?) {
+                        label.foreground = UIManager.getColor("Label.foreground")
+                    }
+
+                    override fun mouseReleased(e: MouseEvent) {
+                        if (SwingUtilities.isLeftMouseButton(e) &&
+                            e.x >= 0 &&
+                            e.x <= label.width &&
+                            e.y >= 0 &&
+                            e.y <= label.height
+                        ) {
+                            collapseAction.actionPerformed(null)
+                        }
+                    }
+                },
+            )
         }
 
-        private fun updateHeaderCheckbox(category: ProtCategory, properties: List<PropertyFilter>) {
+        private fun updateHeaderCheckbox(
+            category: ProtCategory,
+            properties: List<PropertyFilter>,
+        ) {
             val checkbox = headerCheckboxes[category] ?: return
+
             val active = proxyService.filterSetStore.getActive()
             val allSelected = properties.all { active[it] }
             val allUnselected = properties.none { active[it] }
-            checkbox.state = when {
-                allSelected -> FlatTriStateCheckBox.State.SELECTED
-                allUnselected -> FlatTriStateCheckBox.State.UNSELECTED
-                else -> FlatTriStateCheckBox.State.INDETERMINATE
-            }
+            checkbox.isEnabled = presetsBox.selectedIndex != 0
+            checkbox.state =
+                when {
+                    allSelected -> FlatTriStateCheckBox.State.SELECTED
+                    allUnselected -> FlatTriStateCheckBox.State.UNSELECTED
+                    else -> FlatTriStateCheckBox.State.INDETERMINATE
+                }
         }
 
         internal fun updateAllHeaderCheckboxes() {
@@ -286,7 +309,7 @@ public class FiltersSidePanel(
         private fun createPropertyFilterPanel(
             category: ProtCategory,
             properties: List<PropertyFilter>,
-            property: PropertyFilter
+            property: PropertyFilter,
         ) = JPanel().apply {
             layout = BorderLayout()
             border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
@@ -319,16 +342,16 @@ public class FiltersSidePanel(
                 headerCheckbox.isSelected = selected
             }
         }
-
     }
 
     private companion object {
-        private fun JComponent.wrapWithBorderlessScrollPane() = JScrollPane(this).apply {
-            horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
-            verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS
-            verticalScrollBar.unitIncrement = 16
+        private fun JComponent.wrapWithBorderlessScrollPane() =
+            JScrollPane(this).apply {
+                horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+                verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS
+                verticalScrollBar.unitIncrement = 16
 
-            border = null
-        }
+                border = null
+            }
     }
 }
