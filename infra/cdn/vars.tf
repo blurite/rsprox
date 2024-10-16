f2f81e7630c430328a4bc9bedda4f4e40d016a89
Corey <corey@notmeta.io>
2024-10-16 01:10:44 +0100

    Deploy dependencies during workflow (#32)
    - Add Terraform for creating the cdn
    - New gradle tasks for generating and uploading jar dependencies to cdn
    - New gradle task for uploading artifacts to maven repo in cdn
      - The runelite-launcher depends on these
    - Reduce size of artifacts by excluding some transient dependencies we don't need

---
+++ infra/cdn/vars.tf
@@ -0,0 +1,7 @@
+variable "root_domain" {
+	default = "rsprox.net"
+}
+
+variable "cdn_domain" {
+  default = "cdn.rsprox.net"
+}
--- patch/patch-runelite/build.gradle.kts
+++ patch/patch-runelite/build.gradle.kts
@@ -1,3 +1,15 @@
+plugins {
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
 dependencies {
     implementation(projects.patch)
     implementation(rootProject.libs.inline.logger)
