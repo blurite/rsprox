607eb6163fbf5dd3f7896ababef2a55d8a14d0d1
HowSpooky <25248588+HowSpooky@users.noreply.github.com>
2024-11-09 15:37:33 +0000

    feat: cache static files for 365 days

--- web/src/main/kotlin/net/rsprox/web/config/WebConfig.kt
+++ web/src/main/kotlin/net/rsprox/web/config/WebConfig.kt
@@ -1,10 +1,12 @@
 package net.rsprox.web.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.resource.VersionResourceResolver
+import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableWebMvc
@@ -12,8 +14,9 @@ public class WebConfig : WebMvcConfigurer {
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry
-            .addResourceHandler("/static/**")
+            .addResourceHandler("/**")
             .addResourceLocations("classpath:/static/")
+            .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
             .resourceChain(true)
             .addResolver(VersionResourceResolver().addContentVersionStrategy("/**"))
     }
