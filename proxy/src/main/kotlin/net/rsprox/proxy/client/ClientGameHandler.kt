c29713ce7b63263f1229ee54b78d2f817343fff6
Kris <kriszenyte@gmail.com>
2025-02-18 10:36:49 +0200

    feat: erase keyboard presses and bank pins on 229

--- proxy/src/main/kotlin/net/rsprox/proxy/client/ClientGameHandler.kt
+++ proxy/src/main/kotlin/net/rsprox/proxy/client/ClientGameHandler.kt
@@ -51,15 +51,15 @@ public class ClientGameHandler(
                         .toJagByteBuf()
                 for (i in 0..<count) {
                     // Note(revision): These buffer methods change
-                    buffer.g1Alt2() // Key
-                    val delta = buffer.g3Alt2()
+                    val delta = buffer.g3()
+                    buffer.g1() // Key
 
                     // Erase any keypresses in general
                     // There is no value in keeping key presses in the logs, at best it helps bot developers,
                     // at worst it leaks sensitive information like private message contents,
                     // which could be re-assembled by going through keyboard events.
-                    replacement.p1Alt2(0)
-                    replacement.p3Alt2(delta)
+                    replacement.p3(delta)
+                    replacement.p1(0)
                 }
 
                 msg.replacePayload(replacement.buffer)
