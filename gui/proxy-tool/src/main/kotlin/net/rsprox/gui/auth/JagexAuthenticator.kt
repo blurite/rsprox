0a6bbdb6e3b49ee220c322e75ed48bf9065343d5
grian <grianguy32@gmail.com>
2025-04-11 02:03:19 +0300

    fix: fix linux browser opening

--- gui/proxy-tool/src/main/kotlin/net/rsprox/gui/auth/JagexAuthenticator.kt
+++ gui/proxy-tool/src/main/kotlin/net/rsprox/gui/auth/JagexAuthenticator.kt
@@ -1,8 +1,13 @@
 package net.rsprox.gui.auth
 
+import com.github.michaelbull.logging.InlineLogger
 import java.awt.Desktop
 import java.net.URI
 import java.util.concurrent.CompletableFuture
+import javax.swing.SwingUtilities
+import kotlin.io.path.Path
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.name
 
 public class JagexAuthenticator {
     private val httpServer = AuthHttpServer()
@@ -18,7 +23,23 @@ public class JagexAuthenticator {
                 "&prompt=login" +
                 "&scope=openid+offline"
         val future = httpServer.waitForResponse(state)
-        Desktop.getDesktop().browse(URI(url))
+        if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            SwingUtilities.invokeLater {
+                Desktop.getDesktop().browse(URI(url))
+            }
+        } else if (System.getProperty("os.name").contains("Linux")) {
+            // doesn't support more than one user as it's fairly impossible to know which user is trying to run since
+            // you only have root context.
+            var username = ""
+            try {
+                username = Path("/home/").listDirectoryEntries()[0].name.replace("/home/", "")
+            } catch (e: IndexOutOfBoundsException) {
+                logger.error { "No linux users found." }
+            }
+            Runtime.getRuntime().exec("sudo --user $username xdg-open $url")
+        } else {
+            error("Unsupported OS for opening browser")
+        }
         return future
     }
 
@@ -30,5 +51,6 @@ public class JagexAuthenticator {
     private companion object {
         const val AUTH_ENDPOINT = "https://account.jagex.com/oauth2/auth"
         const val CLIENT_ID = "1fddee4e-b100-4f4e-b2b0-097f9088f9d2"
+        private val logger = InlineLogger()
     }
 }
