d4f6d17a25c5fb01f5d5ae1064dacc058fb55306
Kris <kriszenyte@gmail.com>
2026-08-04 16:16:47 +0300

    feat: revision 240 (initial copy of 239)

---
+++ protocol/osrs-240/src/main/kotlin/net/rsprox/protocol/v240/game/outgoing/decoder/codec/sound/AmbientSoundStartDecoder.kt
@@ -0,0 +1,24 @@
+package net.rsprox.protocol.v240.game.outgoing.decoder.codec.sound
+
+import net.rsprot.buffer.JagByteBuf
+import net.rsprot.protocol.ClientProt
+import net.rsprox.protocol.ProxyMessageDecoder
+import net.rsprox.protocol.game.outgoing.model.sound.AmbientSoundStart
+import net.rsprox.protocol.session.Session
+import net.rsprox.protocol.v240.game.outgoing.decoder.prot.GameServerProt
+
+internal class AmbientSoundStartDecoder : ProxyMessageDecoder<AmbientSoundStart> {
+    override val prot: ClientProt = GameServerProt.AMBIENTSOUND_START
+
+    override fun decode(
+        buffer: JagByteBuf,
+        session: Session,
+    ): AmbientSoundStart {
+        val fade = buffer.g1Alt1() == 1
+        val id = buffer.g2()
+        return AmbientSoundStart(
+            id,
+            fade,
+        )
+    }
+}
---
+++ protocol/osrs-240/src/main/kotlin/net/rsprox/protocol/v240/game/outgoing/decoder/codec/sound/AmbientSoundStopDecoder.kt
@@ -0,0 +1,22 @@
+package net.rsprox.protocol.v240.game.outgoing.decoder.codec.sound
+
+import net.rsprot.buffer.JagByteBuf
+import net.rsprot.protocol.ClientProt
+import net.rsprox.protocol.ProxyMessageDecoder
+import net.rsprox.protocol.game.outgoing.model.sound.AmbientSoundStop
+import net.rsprox.protocol.session.Session
+import net.rsprox.protocol.v240.game.outgoing.decoder.prot.GameServerProt
+
+internal class AmbientSoundStopDecoder : ProxyMessageDecoder<AmbientSoundStop> {
+    override val prot: ClientProt = GameServerProt.AMBIENTSOUND_STOP
+
+    override fun decode(
+        buffer: JagByteBuf,
+        session: Session,
+    ): AmbientSoundStop {
+        val fade = buffer.g1() == 1
+        return AmbientSoundStop(
+            fade,
+        )
+    }
+}
