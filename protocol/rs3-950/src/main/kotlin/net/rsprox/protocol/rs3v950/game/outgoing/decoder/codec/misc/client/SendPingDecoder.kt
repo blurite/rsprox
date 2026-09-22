618ebe51141f51af6cfc46172d42fa310c312966
Kris <kriszenyte@gmail.com>
2026-09-22 23:35:04 +0300

    fix: rs3 send_ping echo values decoding

--- protocol/rs3-950/src/main/kotlin/net/rsprox/protocol/rs3v950/game/outgoing/decoder/codec/misc/client/SendPingDecoder.kt
+++ protocol/rs3-950/src/main/kotlin/net/rsprox/protocol/rs3v950/game/outgoing/decoder/codec/misc/client/SendPingDecoder.kt
@@ -2,11 +2,13 @@ package net.rsprox.protocol.rs3v950.game.outgoing.decoder.codec.misc.client
 
 import net.rsprot.buffer.JagByteBuf
 import net.rsprot.protocol.ClientProt
+import net.rsprot.protocol.metadata.Consistent
 import net.rsprox.protocol.ProxyMessageDecoder
 import net.rsprox.protocol.rs3.game.outgoing.model.misc.client.SendPing
 import net.rsprox.protocol.rs3v950.game.outgoing.decoder.prot.GameServerProt
 import net.rsprox.protocol.session.Session
 
+@Consistent
 internal class SendPingDecoder : ProxyMessageDecoder<SendPing> {
     override val prot: ClientProt = GameServerProt.SEND_PING
 
@@ -14,8 +16,8 @@ internal class SendPingDecoder : ProxyMessageDecoder<SendPing> {
         buffer: JagByteBuf,
         session: Session,
     ): SendPing {
-        val echo0 = buffer.g4Alt1()
-        val echo1 = buffer.g4Alt1()
+        val echo0 = buffer.g4()
+        val echo1 = buffer.g4()
         return SendPing(
             echo0,
             echo1,
